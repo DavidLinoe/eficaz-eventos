622b415f8cbf01a1f963cc66d08ff32d75ea3474
DavidLinoe <davideduardolino@outlook.com>
2026-05-11 18:00:50 -0300

    feat: implementando tela e listagem das inscricoes

--- resources/views/registrations.blade.php
+++ resources/views/registrations.blade.php
@@ -1,7 +1,41 @@
 <x-layout>
 
-    <div class="text-blue-600">
-        Tela de Inscrições
+    <div class="flex flex-col gap-6 p-6 max-w-2xl mx-auto">
+
+        <div class="text-blue-600">
+            Minhas Inscrições
+        </div>
+
+        @if ($errors->any())
+            <ul>
+                @foreach ($errors->all() as $error)
+                    <li>{{ $error }}</li>
+                @endforeach
+            </ul>
+        @endif
+
+        <div class="flex flex-col gap-2">
+            <ul class="flex flex-col gap-2">
+                @forelse ($registrations as $registration)
+                    <li class="flex items-center gap-3">
+                        <span>
+                            {{ $registration->event->title }} —
+                            {{ $registration->event->starts_at?->format('d/m/Y H:i') }}
+                        </span>
+                        <form action="{{ route('events.unregister', $registration->event) }}" method="POST">
+                            @csrf
+                            @method('DELETE')
+                            <button type="submit">cancelar inscrição</button>
+                        </form>
+                    </li>
+                @empty
+                    <li>Você ainda não tem inscrições.</li>
+                @endforelse
+            </ul>
+        </div>
+
+        <a href="{{ route('events.index') }}">voltar para eventos</a>
+
     </div>
 
 </x-layout>

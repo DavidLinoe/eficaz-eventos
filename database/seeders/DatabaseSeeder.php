651f93bee4489f18eb7173936098956fc6bc3adc
DavidLinoe <davideduardolino@outlook.com>
2026-05-11 15:04:11 -0300

    feat: declarando as seed no database seeder

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -21,5 +21,15 @@ class DatabaseSeeder extends Seeder
             'name' => 'Test User',
             'email' => 'test@example.com',
         ]);
+
+
+        $this->call([
+            EventSeeder::class,
+        ]);
+
+
+        $this->call([
+            RegistrationSeeder::class,
+        ]);
     }
 }
